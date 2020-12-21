265afbcfb52119d1f4e775da1cf8b732e6c16c8c
charles <charles@192.168.1.15>
2020-12-21 15:43:58 +0100

    rajout de tests

--- src/test/ProduitDaoImplTest.java
+++ src/test/ProduitDaoImplTest.java
@@ -60,10 +60,10 @@ class ProduitDaoImplTest {
 		HashMap<Integer, Produit> produitsExpected = new HashMap<Integer, Produit>();
 		Produit p1 = new Produit(6, "Tout JavaScript", 10, "Ce livre s’adresse à tous les développeurs web, qu’ils soient  débutants ou avancés."
 				+ " Le JavaScript sert avant tout à rendre les pages web interactives  et dynamiques du côté de l’utilisateur, mais il est  également de plus en plus utilisé pour créer des applications  complètes, y compris côté serveur",
-                "urlimage", 2, 22.9);
+                "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/tout%20javascript.jpg", 2, 22.9);
 		Produit p2 = new Produit(8, "Le tour du monde en 80 jours", 85, "En 1872, un riche gentleman londonien, Phileas Fogg, parie vingt mille livres qu'il fera le tour du monde en quatre-vingts jours. "
 				+ "Accompagné de son valet de chambre, le dévoué Passepartout, il quitte Londres pour une formidable course contre la montre. Au prix de mille aventures, notre héros va s'employer à gagner ce pari.",
-	            "urlimage", 1, 5.5);
+	            "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/le%20tour%20du%20monde%20en%2080%20jours.jpg", 1, 5.5);
 		produitsExpected.put(1, p1);
 		produitsExpected.put(2, p2);
 		ProduitDao dao = new ProduitDaoImpl();
@@ -76,7 +76,7 @@ class ProduitDaoImplTest {
 	void testFindByPrice() {
 		HashMap<Integer, Produit> produitsExpected = new HashMap<Integer, Produit>();
 		Produit p = new Produit(7, "Mémento CSS3", 36, "Aussi léger que riche, indéchirable et imperméable, voici la 4e édition mise à jour du mémento CSS 3 de Raphaël Goetter, avec une lisibilité améliorée !",
-	            "urlimage", 2, 5);
+	            "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/memento%20CSS3.jpg", 2, 5);
 		produitsExpected.put(1, p);
 		ProduitDao dao = new ProduitDaoImpl();
 		HashMap<Integer, Produit> actual = dao.findByPrice(5);
@@ -88,11 +88,11 @@ class ProduitDaoImplTest {
 	void testFindByCategorie() {
 		HashMap<Integer, Produit> produitsExpected = new HashMap<Integer, Produit>();
 		Produit p1 = new Produit(14, "One Piece - Édition originale - Tome 01: À l'aube d'une grande aventure", 50, "Nous sommes à l'ère des pirates. Luffy, un garçon espiègle, rêve de devenir le roi des pirates en trouvant le “One Piece”, un fabuleux trésor. Seulement, Luffy a avalé un fruit du démon qui l'a transformé en homme élastique.",
-	            "urlimage", 3, 6.9);
+	            "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/One%20Piece%20-%20%C3%89dition%20originale%20-%20Tome%2001.jpg", 3, 6.9);
 		Produit p2 = new Produit (15, "One piece - Edition originale Vol.2", 32, "Luffy atterrit sur une île occupée par Baggy et son groupe, un équipage de pirates redoutable. Il y rencontre Nami, une « voleuse de pirates ». Apprenant la nature de Luffy, elle décide de l’utiliser pour infiltrer les rangs de Baggy et dérober ses trésors…",
-	            "urlimage", 3, 6.9);
+	            "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/One%20piece%20-%20Edition%20originale%20Vol.2%20.jpg", 3, 6.9);
 		Produit p3 = new Produit(16, "One Piece - Édition originale - Tome 03: Une vérité qui blesse", 0, "Luffy affronte Baggy. Ce dernier admet vouer une haine sans nom envers Shanks. En effet, tous deux ont appris les rudiments de la piraterie sur le même navire, et un malencontreux incident a provoqué la descente aux enfers de Baggy…",
-	            "urlimage", 3, 6.9);
+	            "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/One%20Piece%20-%20%C3%89dition%20originale%20-%20Tome%2003%20Une%20v%C3%A9rit%C3%A9%20qui%20blesse.jpg", 3, 6.9);
 		produitsExpected.put(1, p1);
 		produitsExpected.put(2, p2);
 		produitsExpected.put(3, p3);
@@ -106,7 +106,7 @@ class ProduitDaoImplTest {
 	void testFindByNameCategorie() {
 		HashMap<Integer, Produit> produitsExpected = new HashMap<Integer, Produit>();
 		Produit p1 = new Produit(14, "One Piece - Édition originale - Tome 01: À l'aube d'une grande aventure", 50, "Nous sommes à l'ère des pirates. Luffy, un garçon espiègle, rêve de devenir le roi des pirates en trouvant le “One Piece”, un fabuleux trésor. Seulement, Luffy a avalé un fruit du démon qui l'a transformé en homme élastique.",
-	            "urlimage", 3, 6.9);
+	            "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/One%20Piece%20-%20%C3%89dition%20originale%20-%20Tome%2001.jpg", 3, 6.9);
 		produitsExpected.put(1, p1);
 		ProduitDao dao = new ProduitDaoImpl();
 		HashMap<Integer, Produit> actual = dao.findByNameCategorie("aventure", 3);
@@ -118,7 +118,7 @@ class ProduitDaoImplTest {
 	void testFindByNamePrice() {
 		HashMap<Integer, Produit> produitsExpected = new HashMap<Integer, Produit>();
 		Produit p1 = new Produit (2, "Programmer en Java", 29, "De la programmation objet en Java au développement d'applications web. Dans cet ouvrage, Claude Delannoy applique au langage Java la démarche pédagogique qui a fait le succès de ses livres sur le C et le C++. Il insiste tout particulièrement sur la bonne compréhension des concepts objet et sur l'acquisition de méthodes de programmation rigoureuses.",
-	            "urlimage", 2, 12.69);
+	            "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/Programmer%20en%20Java%20.jpg", 2, 12.69);
 		produitsExpected.put(1, p1);
 		ProduitDao dao = new ProduitDaoImpl();
 		HashMap<Integer, Produit> actual = dao.findByNamePrice("java", 15);
@@ -130,7 +130,7 @@ class ProduitDaoImplTest {
 	void testFindByPriceCategorie() {
 		HashMap<Integer, Produit> produitsExpected = new HashMap<Integer, Produit>();
 		Produit p1 = new Produit  (8, "Le tour du monde en 80 jours", 85, "En 1872, un riche gentleman londonien, Phileas Fogg, parie vingt mille livres qu'il fera le tour du monde en quatre-vingts jours. Accompagné de son valet de chambre, le dévoué Passepartout, il quitte Londres pour une formidable course contre la montre. Au prix de mille aventures, notre héros va s'employer à gagner ce pari.",
-	            "urlimage", 1, 5.5);
+	            "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/le%20tour%20du%20monde%20en%2080%20jours.jpg", 1, 5.5);
 		produitsExpected.put(1, p1);
 		ProduitDao dao = new ProduitDaoImpl();
 		HashMap<Integer, Produit> actual = dao.findByPriceCategorie(1, 6);
@@ -142,7 +142,7 @@ class ProduitDaoImplTest {
 	void testFindByAll() {
 		HashMap<Integer, Produit> produitsExpected = new HashMap<Integer, Produit>();
 		Produit p1 = new Produit (4, "Voyage au centre de la Terre", 56, "Le professeur Lidenbrock est persuadé d'avoir découvert le chemin qui mène au centre de la Terre. Accompagné de son neveu Axel, l'impétueux géologue part en Islande. Là, au fond d'un volcan, les deux explorateurs et leur guide s'enfoncent dans les entrailles mystérieuses du globe. Un voyage d'une folle audace, véritable défi lancé à la science.",
-	            "urlimage", 1, 9.99);
+	            "https://raw.githubusercontent.com/edwarbarrera/C-Users-aline-eclipse-workspace-projet_cda_1/master/WebContent/ressources/img/Voyage%20au%20centre%20de%20la%20Terre.jpg", 1, 9.99);
 		produitsExpected.put(1, p1);
 		ProduitDao dao = new ProduitDaoImpl();
 		HashMap<Integer, Produit> actual = dao.findByAll("voyage", 10, 1);
